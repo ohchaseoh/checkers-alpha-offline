a6f019b047e6ce684a3b129d5b8daea6d5dcdd39
msurur <73732750+msurur@users.noreply.github.com>
2022-11-30 22:49:52 -0800

    dumb ai impl
    dumb ai

--- app/src/main/java/com/example/game/Checkers/info/CheckerState.java
+++ app/src/main/java/com/example/game/Checkers/info/CheckerState.java
@@ -11,11 +11,79 @@ import java.io.Serializable;
 public class CheckerState extends GameState implements Serializable{
     public static final int MAX_PLAYERS = 2;
 
+
+    private char[][] board;
+
+    private int playerToMove;
+
     public CheckerState() {
         super();
+        board = new char[8][8];
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                board[i][j] = ' ';
+            }
+        }
+        playerToMove = 0;
     }
 
     public CheckerState(CheckerState orig) {
         super();
+        board = new char[8][8];
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                board[i][j] = orig.board[i][j];
+            }
+        }
+
+        playerToMove = orig.playerToMove;
+        super.numSetupTurns = orig.numSetupTurns;
+        super.currentSetupTurn = orig.currentSetupTurn;
+    }
+
+    public char getPiece(int row, int col) {
+        // if we're out of bounds or anything, return '?';
+        if (board == null || row < 0 || col < 0) return '?';
+        if (row >= board.length || col >= board[row].length) return '?';
+
+        // return the character that is in the proper position
+        return board[row][col];
+    }
+
+    public void setPiece(int row, int col, char piece) {
+        // if we're out of bounds or anything, return;
+        if (board == null || row < 0 || col < 0) return;
+        if (row >= board.length || col >= board[row].length) return;
+
+        // return the character that is in the proper position
+        board[row][col] = piece;
+    }
+
+    //Tells whos move it is
+    public int getWhoseMove() {
+        return playerToMove;
+    }
+
+    //Set whos move it is
+    public void setWhoseMove(int id) {
+        playerToMove = id;
+    }
+
+
+    public boolean equals(Object object){
+        if(! (object instanceof CheckerState)) return false;
+        CheckerState checkerState = (CheckerState) object;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(this.board[i][j] != CheckerState.board[i][j]){
+                    return false;
+                }
+            }
+        }
+
+        if (this.playerToMove != CheckerState.playerToMove || this.numSetupTurns != CheckerState.numSetupTurns || this.currentSetupTurn != CheckerState.currentSetupTurn){
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/game/Checkers/players/CheckersComputerPlayer.java
+++ app/src/main/java/com/example/game/Checkers/players/CheckersComputerPlayer.java
@@ -1,7 +1,14 @@
 package com.example.game.Checkers.players;
 
+import com.example.game.Checkers.BlackChecker;
+import com.example.game.Checkers.Checker;
+import com.example.game.Checkers.RedChecker;
+import com.example.game.Checkers.info.CheckerState;
 import com.example.game.GameFramework.info.GameInfo;
 import com.example.game.GameFramework.players.GameComputerPlayer;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Represents the Checkers AI computer player and contains all the logic for it to
@@ -25,9 +32,55 @@ public class CheckersComputerPlayer extends GameComputerPlayer {
         super(name);
         this.isSmart =  isSmart;
     }
-
+//red = 1
+//black = 0
     @Override
     protected void receiveInfo(GameInfo info) {
-        
+        //all the pieces that can move on the computer side of the game
+        //
+        ArrayList<Object> availablePieces = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            for(int k = 0; k < 8; k++){
+                if (CheckerState.checkerList(i, k) == 1){
+                    return;
+                }
+                if (CheckerState.board(i, k) == 3){
+                    sleep(1);
+                }
+                Checker c = CheckerState.getPiece(i, k);
+                if (playerNum == 0 && c.pieceColor() == Piece.ColorType.RED){
+                    availablePieces.add(c);
+                } else if (playerNum == 1 && c.pieceColor() == Piece.ColorType.BLACK) {
+                    availablePieces.add(c);
+                }
+//black and red each checker doesn't have attached color assigned to it. both red and black are checkers don't have colors assigned to them.
+                    //reference image button,
+            }
+        }
+
+        //shuffles checkers in the array
+        Collections.shuffle(availablePieces);
+        selection = availablePieces.get(0);
+        for (int i = 1; i < availablePieces.size(); i++){
+            if (checkMove(selection, CheckerState)){
+                selection = availablePieces.get(i);
+            }
+        }
+
+        int xVal = selection.getX();
+        int yVal = selection.getY();
+
+        xVal = BlackChecker.getX().get(ints.get(0));
+        yVal = RedChecker.getY().get(ints.get(0));
+        if (selection.getPieceColor() == Piece.ColorType.BLACK){
+            if (yVal == 7){
+                sendPromotionAction(xVal, yVal, Piece.ColorType.BLACK);
+            }
+        } else if (selection.getPieceColor() == Piece.ColorType.RED){
+            if (yVal == 0) {
+                sendPromotionAction(xVal, yVal, Piece.ColorType.RED;
+            }
+        }
+        game.sendAction(new CheckerMoveAction(this, xVal, yVal));
     }
 }
